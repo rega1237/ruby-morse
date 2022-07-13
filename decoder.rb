cf532ee10514b762304e9a78b90c75c3d3d386f1
Rafael Guzman <rega1237@gmail.com>
2022-07-13 19:31:07 -0400

    fix linters

--- decoder.rb
+++ decoder.rb
@@ -1,58 +1,56 @@
-Morse_table = {
-  ".-" => "a",
-  "-..." => "b",
-  "-.-." => "c",
-  "-.." => "d",
-  "." => "e",
-  "..-." => "f",
-  "--." => "g",
-  "...." => "h",
-  ".." => "i",
-  ".---" => "j",
-  "-.-" => "k",
-  ".-.." => "l",
-  "--" => "m",
-  "-." => "n",
-  "---" => "o",
-  ".--." => "p",
-  "--.-" => "q",
-  ".-." => "r",
-  "..." => "s",
-  "-" => "t",
-  "..-" => "u",
-  "...-" => "v",
-  ".--" => "w",
-  "-..-" => "x",
-  "-.--" => "y",
-  "--.." => "z",
-}
+MORSE_TABLE = {
+  '.-' => 'a',
+  '-...' => 'b',
+  '-.-.' => 'c',
+  '-..' => 'd',
+  '.' => 'e',
+  '..-.' => 'f',
+  '--.' => 'g',
+  '....' => 'h',
+  '..' => 'i',
+  '.---' => 'j',
+  '-.-' => 'k',
+  '.-..' => 'l',
+  '--' => 'm',
+  '-.' => 'n',
+  '---' => 'o',
+  '.--.' => 'p',
+  '--.-' => 'q',
+  '.-.' => 'r',
+  '...' => 's',
+  '-' => 't',
+  '..-' => 'u',
+  '...-' => 'v',
+  '.--' => 'w',
+  '-..-' => 'x',
+  '-.--' => 'y',
+  '--..' => 'z'
+}.freeze
 
 def decode_char(char)
- @char = ''
- Morse_table.each {|key,value|
-   if key == char
-     @char = value
-   end
- }
- return @char
+  @char = ''
+  MORSE_TABLE.each do |key, value|
+    @char = value if key == char
+  end
+  @char
 end
 
 def decode_word(word)
- @ar_word = word.split
- @word = ''
-   @ar_word.each {|morse|
-     @word += "#{decode_char(morse)}"
-   }
- return @word
+  @ar_word = word.split
+  @word = ''
+  @ar_word.each do |morse|
+    @word += decode_char(morse).to_s
+  end
+  @word
 end
 
 def decode(message)
- @ar_message = message.split("   ")
- @message = ''
- @ar_message.each {|message|
-   @message += "#{decode_word(message)} "
- }
- return @message
+  @ar_message = message.split('   ')
+  @out_message = ''
+  @ar_message.each do |morse_message|
+    @out_message += "#{decode_word(morse_message)} "
+  end
+  @out_message
 end
 
 puts decode_char('.-')
